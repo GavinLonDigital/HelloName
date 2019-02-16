b9a1ddf1a9d8cf24247a7f4a244ff5b8aafbf4e1
Lon <gavinlondee@gmail.com>
2019-02-16 10:43:59 +0200

    Initial commit

---
+++ HelloName/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name");
+
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"Hello {name}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
